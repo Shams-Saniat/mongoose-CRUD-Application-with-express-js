78ab734c82eb52863cb5de63c1782baf5a108539
Shams Saniat <51787712+Shams-Saniat@users.noreply.github.com>
2025-04-17 16:42:45 +0600

    Created Mongoose Model

--- routeHnadler/todoHandler.js
+++ routeHnadler/todoHandler.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
+const todoSchema = require('../schemas/todoSchema');
+const ToDo = new mongoose.model("Todo", todoSchema);
+
 
 // GET ALL THE TODOS
 router.get("/", async (req, res) => {});
@@ -7,8 +11,10 @@ router.get("/", async (req, res) => {});
 // GET A TODO by ID
 router.get("/:id", async (req, res) => {});
 
-// POST TODO
-router.post("/", async (req, res) => {});
+// POST A TODO
+router.post("/", async (req, res) => {
+    const newToDo = new ToDo
+});
 
 // POST MULTIPLE TODOS
 router.post("/all", async (req, res) => {});
