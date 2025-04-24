d569782191dd8c3b675c2a487b6fd9ac0f229a2a
Shams Saniat <51787712+Shams-Saniat@users.noreply.github.com>
2025-04-24 14:57:20 +0600

    Added functionality for getting match records - find()

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -2,20 +2,61 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 const todoSchema = require("../schemas/todoSchema");
-const ToDo = new mongoose.model("Todo", todoSchema);
 
-// GET ALL THE TODOS
-router.get("/", async (req, res) => {});
+const Todo = mongoose.model("Todo", todoSchema);
+
+// GET ALL THE ACTIVE TODOS
+router.get("/", async (req, res) => {
+  try {
+    const data = await Todo.find({ status: "active" })
+      .select({ _id: 0, __v: 0, date: 0 })
+      .limit(2);
+
+    res.status(200).json({
+      result: data,
+      message: "Success",
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: "There was a server side error!",
+      details: err.message,
+    });
+  }
+});
 
 // GET A TODO by ID
-router.get("/:id", async (req, res) => {});
+router.get("/:id", async (req, res) => {
+  try {
+    const data = await Todo.findById(req.params.id).select({
+      _id: 0,
+      __v: 0,
+      date: 0,
+    });
+
+    if (data) {
+      res.status(200).json({
+        result: data,
+        message: "Success",
+      });
+    } else {
+      res.status(404).json({
+        error: "ToDo not found!",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      error: "There was a server side error!",
+      details: err.message,
+    });
+  }
+});
 
 // POST A TODO
 router.post("/", async (req, res) => {
   try {
-    const newToDo = new ToDo(req.body);
-    await newToDo.save();
-    res.status(200).json({
+    const newTodo = new Todo(req.body);
+    await newTodo.save();
+    res.status(201).json({
       message: "ToDo was inserted successfully!",
     });
   } catch (err) {
@@ -29,8 +70,8 @@ router.post("/", async (req, res) => {
 // POST MULTIPLE TODOS
 router.post("/all", async (req, res) => {
   try {
-    await ToDo.insertMany(req.body); // <-- this handles arrays
-    res.status(200).json({
+    await Todo.insertMany(req.body); // expects an array
+    res.status(201).json({
       message: "ToDos were inserted successfully!",
     });
   } catch (err) {
@@ -44,21 +85,21 @@ router.post("/all", async (req, res) => {
 // PUT TODO
 router.put("/:id", async (req, res) => {
   try {
-    const result = await ToDo.findByIdAndUpdate(
-      { _id: req.params.id },
-      {
-        $set: {
-          status: "active",
-        },
-      },
-      {
-        new: true,
-        useFindAndModify: false,
-      },
+    const result = await Todo.findByIdAndUpdate(
+      req.params.id,
+      { $set: { status: "active" } },
+      { new: true }
     );
-    res.status(200).json({
-      message: "ToDo was updated successfully!",
-    });
+
+    if (result) {
+      res.status(200).json({
+        message: "ToDo was updated successfully!",
+      });
+    } else {
+      res.status(404).json({
+        error: "ToDo not found!",
+      });
+    }
   } catch (err) {
     res.status(500).json({
       error: "There was a server side error!",
@@ -66,9 +107,7 @@ router.put("/:id", async (req, res) => {
     });
   }
 });
-console.log(result);
 
-// DELETE TODO
-router.delete("/:id", async (req, res) => {});
+
 
 module.exports = router;
