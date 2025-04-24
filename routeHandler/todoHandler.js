681e7dada9132b789c44b209ac45668289984ae5
Shams Saniat <51787712+Shams-Saniat@users.noreply.github.com>
2025-04-24 14:58:24 +0600

    Added functionality for delete a single record - delete()

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -108,6 +108,26 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+// DELETE TODO
+router.delete("/:id", async (req, res) => {
+  try {
+    const result = await Todo.deleteOne({ _id: req.params.id });
 
+    if (result.deletedCount > 0) {
+      res.status(200).json({
+        message: "ToDo was deleted successfully",
+      });
+    } else {
+      res.status(404).json({
+        error: "ToDo not found!",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      error: "There was a server side error!",
+      details: err.message,
+    });
+  }
+});
 
 module.exports = router;
